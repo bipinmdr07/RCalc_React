df67419a9bc4fb2ccb6e57ebc73d2f78f6d43f32
Bipin Manandhar <kubipin07@gmail.com>
2018-04-12 20:18:06 +0545

    Add react state for handling user form input

--- app/assets/javascripts/components/appointments.jsx
+++ app/assets/javascripts/components/appointments.jsx
@@ -1,7 +1,23 @@
-window.Appointments = createReactClass({
+var Appointments = createReactClass({
+
+  getInitialState: function() {
+    return {
+      appointments: this.props.appointments,
+      title: 'Team Startup Meeting',
+      appt_time: 'Tomorrow at 9am'
+    }
+  },
+
+  handleUserInput: function(obj){
+    this.setState(obj);
+  },
+
   render: function() {
     return (
-      <h1>Cal React</h1>
+      <div>
+        <AppointmentForm input_title={this.state.input_title} input_appt_time={this.state.input_appt_time} onUserInput={this.handleUserInput}/>
+        <AppointmentList appointments={this.state.appointments} />
+      </div>
     )
   }
 });
