d2ff46f20d40213115df74734395de083fc957a6
Bipin Manandhar <kubipin07@gmail.com>
2018-04-14 07:49:14 +0545

    Add third-party React component for date-picker

--- app/assets/javascripts/components/appointment_form.jsx
+++ app/assets/javascripts/components/appointment_form.jsx
@@ -11,7 +11,18 @@ var AppointmentForm = createReactClass({
     this.props.onFormSubmit();
   },
 
+  setApptTime: function(e){
+    var name = 'appt_time';
+    var obj = {};
+    if (obj[name] = e.toDate()){
+      this.props.onUserInput(obj);
+    }
+  },
+
   render: function(){
+    var inputProps = {
+      name: 'appt_time'
+    };
     return (
       <div>
         <h2>Make a new appointment.</h2>
@@ -19,10 +30,11 @@ var AppointmentForm = createReactClass({
           <input name='title' placeholder='Appointment Title'
             value={this.props.title}
             onChange={this.handleChange} />
-          <input name='appt_time' placeholder='Date and Time'
-            value={this.props.appt_time}
-            onChange={this.handleChange} />
-          <input type='submit' value='Make Appointment' />
+
+          <Datetime input = {false} open = {true} inputProps={inputProps} value={this.props.appt_time} onChange={this.setApptTime}/>
+
+
+          <input type='submit' value='Make Appointment'/>
         </form>
       </div>
     )
